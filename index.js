8d8503a6c9bccbb1d44328898bb38034b9bd714d
Christopher Dyer <me@christopherdyer.net>
2018-01-17 15:23:13 +1100

    Thunderbirds are Go
    - Get args from command line
    - Support for splitting of files by entry len
    - Using streams for CSV is super-fast

---
+++ index.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+const { argv } = require('yargs');
+const csv = require('csv-parser');
+const through2 = require('through2');
+const process = require('process');
+const fs = require('fs-extra');
+const path = require('path');
+
+const filterDebugger = require('debug')('Filter');
+const writerDebugger = require('debug')('Filter');
+
+const csvFile = argv.infile;
+const outFile = argv.outfile;
+const outFileMaxEntries = argv.maxlen || null;
+
+if (!csvFile) {
+  console.error(`Usage: ${argv.$0} --infile=<filename>`);
+  process.exit(1);
+}
+
+const emails = new Set();
+let processedRowCount = 0;
+
+const responseFilter = {
+  field: 'attributes.interaction.response.0.value',
+  value: 'Yes',
+};
+
+console.info(`Opening file ${csvFile}`);
+
+const recordCounter = through2.obj((row, enc, cb) => {
+  processedRowCount += 1;
+  cb(null, row);
+});
+
+/**
+ * @param {Object} filter
+ * @param {String}        filter.field
+ * @param {Number|String} filter.name
+ */
+const recordFilter = (filter) => {
+  let firstLine = true;
+  filterDebugger(`Filtering records on ${filter.field} === ${filter.value}`);
+  return through2.obj(function (appcuesEvent, enc, cb) { // eslint-disable-line func-names
+    if (!emails.has(appcuesEvent.user_id)
+    && appcuesEvent[filter.field] === filter.value
+    && appcuesEvent.name === 'appcues:form_submitted') {
+      this.push(`${firstLine ? '' : ','}${appcuesEvent.user_id}`);
+      emails.add(appcuesEvent.user_id);
+      firstLine = false;
+    }
+    cb();
+  });
+};
+
+/**
+ * @param {String}      outFileName
+ * @param {Number|null} maxBatchLen
+ */
+const batchedWriter = (outFileName, maxBatchLen = null) => {
+  if (!maxBatchLen) {
+    return fs.createWriteStream(outFileName);
+  }
+  writerDebugger(`Creating batches of ${maxBatchLen} files`);
+  const fileStreams = [];
+  let currFileLen = 0;
+  let currFileIndex = 0;
+  const genFileName = i => outFileName.replace(path.extname(outFileName), `.${i}${path.extname(outFileName)}`);
+  let ws = fs.createWriteStream(genFileName(currFileIndex));
+  fileStreams.push(ws);
+  return through2((chunk, enc, cb) => {
+    if (currFileLen > maxBatchLen) {
+      currFileIndex += 1;
+      currFileLen = 0;
+      fileStreams.push(ws = fs.createWriteStream(genFileName(currFileIndex)));
+    }
+    ws.write(currFileLen === 0 ? chunk.toString().replace(',', '') : chunk); // omit leading comma for new files
+    currFileLen += 1;
+    cb();
+  }, (onFlush) => {
+    fileStreams.forEach(s => s.end());
+    writerDebugger(`Created ${fileStreams.length} file(s)`);
+    onFlush();
+  });
+};
+
+fs.access(csvFile).then(() => {
+  const outputStream = outFile ? batchedWriter(outFile, outFileMaxEntries) : through2((c, e, cb) => { cb(null, c); });
+  fs.createReadStream(csvFile)
+    .pipe(csv())
+    .pipe(recordCounter)
+    .pipe(recordFilter(responseFilter))
+    .pipe(outputStream)
+    .on('finish', () => {
+      console.info(`Processed ${processedRowCount} entries`);
+      console.info(`${emails.size} unique email addresses indicated as Suppliers`);
+      process.exit(0);
+    });
+}).catch(() => {
+  console.error(`File '${csvFile}' does not exist`);
+  process.exit(1);
+});
